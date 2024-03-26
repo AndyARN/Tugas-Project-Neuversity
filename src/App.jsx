2a4d59e2242127ed10e650b0e57a43e7b6fcaecc
ARN <nugrohoandyreza@gmail.com>
2024-03-27 00:45:55 +0700

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 export default function App() {
   return (
     <div className="prose">
-      <h1>Hello world!</h1>
-      <h3>testing</h3>
+      <h1>Berhasil push ke github</h1>
     </div>
   );
 }
