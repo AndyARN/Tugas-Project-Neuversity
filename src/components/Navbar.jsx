d052f1703c233e288f5c0e72e34dbed62bbe686c
ARN <nugrohoandyreza@gmail.com>
2024-03-28 21:36:50 +0700

    create navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,21 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
-      </div>
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            className="inline-block w-5 h-5 stroke-current"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-            ></path>
-          </svg>
-        </button>
+    <div className="bg-white shadow navbar">
+      <div className="container flex-col sm:flex-row">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">MyArticle</a>
+        </div>
+        <div className="flex-none">
+          {/* input search */}
+          <div className="join">
+            <input
+              className="w-full input input-sm sm:w-36 input-bordered join-item"
+              placeholder="Pencarian..."
+            />
+            <button className="btn btn-sm join-item rounded-r-lg">Cari</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/layouts/ClientLayout.jsx
+++ src/layouts/ClientLayout.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function ClientLayout({ children }) {
   return (
     <>
-      <nav>Navbar</nav>
+      <Navbar />
       <main>{children}</main>
       <footer>Footer</footer>
     </>
